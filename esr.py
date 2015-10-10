e5f78b2603a6f102c52e7a8f43d39c4f6a7d2d57
Chris Williams <chrispwill@gmail.com>
2015-10-10 18:28:03 +1100

    Change ESR from class to module

--- esr.py
+++ esr.py
@@ -3,52 +3,55 @@ import sqlite3
 
 from os.path import join
 
+config = configparser.ConfigParser()
+config.read('settings.ini')
+db_loc = join(config['DEFAULT']['db_dir'],
+              config['DEFAULT']['db_filename'])
+use_mem = config['DEFAULT']['use_mem']
+if "yes" in use_mem:
+    file_conn = sqlite3.connect(db_loc)
 
-class ESR:
-    def name_from_typeid(self, typeID):
-        return self.generic_query("invTypes", "typeName", "typeID", typeID)
+    def tf(x):
+        return str(x, 'utf-8', 'ignore')
 
-    def vol_from_typeid(self, typeID):
-        return self.generic_query("invTypes", "volume", "typeID", typeID)
+    file_conn.text_factory = tf
+    conn = sqlite3.connect(':memory:')
 
-    def generic_query(self, table, qcol, col, colval):
-        cursor = self.__cursor()
-        query = "SELECT " + qcol + " FROM " + table + " WHERE " + col + "="
-        if isinstance(colval, (str, bytes)):
-            query += "\"" + colval + "\""
-        else:
-            query += str(colval)
-        query += ";"
-        cursor.execute(query)
-        return cursor.fetchall()
+    query = "".join(line for line in file_conn.iterdump())
+    conn.executescript(query)
+else:
+    conn = sqlite3.connect(db_loc)
 
-    def printtables(self):
-        print(self.__tablelist())
 
-    def __init__(self):
-        config = configparser.ConfigParser()
-        config.read('settings.ini')
-        db_loc = join(config['DEFAULT']['db_dir'],
-                      config['DEFAULT']['db_filename'])
-        use_mem = config['DEFAULT']['use_mem']
-        if "yes" in use_mem:
-            file_conn = sqlite3.connect(db_loc)
+def name_from_typeid(typeID):
+    return generic_query("invTypes", "typeName", "typeID", typeID)
 
-            def tf(x):
-                return str(x, 'utf-8', 'ignore')
 
-            file_conn.text_factory = tf
-            self.conn = sqlite3.connect(':memory:')
+def vol_from_typeid(typeID):
+    return generic_query("invTypes", "volume", "typeID", typeID)
 
-            query = "".join(line for line in file_conn.iterdump())
-            self.conn.executescript(query)
-        else:
-            self.conn = sqlite3.connect(db_loc)
 
-    def __cursor(self):
-        return self.conn.cursor()
+def generic_query(table, qcol, col, colval):
+    cursor = __cursor()
+    query = "SELECT " + qcol + " FROM " + table + " WHERE " + col + "="
+    if isinstance(colval, (str, bytes)):
+        query += "\"" + colval + "\""
+    else:
+        query += str(colval)
+    query += ";"
+    cursor.execute(query)
+    return cursor.fetchall()
 
-    def __tablelist(self):
-        cursor = self.__cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        return cursor.fetchall()
+
+def printtables():
+    print(__tablelist())
+
+
+def __cursor():
+    return conn.cursor()
+
+
+def __tablelist():
+    cursor = __cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    return cursor.fetchall()
