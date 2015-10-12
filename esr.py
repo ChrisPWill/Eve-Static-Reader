704d041168461ada80d50867aed96d0deefa419f
Chris Williams <chrispwill@gmail.com>
2015-10-12 13:23:46 +1100

    I dunno lol

--- esr.py
+++ esr.py
@@ -1,5 +1,6 @@
 def from_typeid(cursor, typeID, colName):
-    (val,) = generic_query(cursor, "invTypes", colName, "typeID", typeID)
+    (val,) = generic_query_fetchone(cursor, "invTypes", colName, "typeID",
+                                    typeID)
     return val
 
 
@@ -11,7 +12,7 @@ def volume_from_typeid(cursor, typeID):
     return from_typeid(cursor, typeID, "volume")
 
 
-def generic_query(cursor, table, qcol, col, colval):
+def generic_query_fetchone(cursor, table, qcol, col, colval):
     query = "SELECT " + qcol + " FROM " + table + " WHERE " + col + "="
     if isinstance(colval, (str, bytes)):
         query += "\"" + colval + "\""
@@ -19,4 +20,4 @@ def generic_query(cursor, table, qcol, col, colval):
         query += str(colval)
     query += ";"
     cursor.execute(query)
-    return cursor.fetchall()
+    return cursor.fetchone()
