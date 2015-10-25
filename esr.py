058cf2ad788df2ad6085ab978f8eedb857a8a9fc
Chris Williams <chrispwill@gmail.com>
2015-10-25 11:06:28 +1100

    Various refinements

--- esr.py
+++ esr.py
@@ -1,6 +1,45 @@
+def op_conv(optype, colval):
+    if optype is 'eq':
+        return {'op': '=', 'val': colval}
+    elif optype is 'contains':
+        return {'op': ' LIKE ', 'val': '%'+colval+'%'}
+    elif optype is 'startswith':
+        return {'op': ' LIKE ', 'val': colval+'%'}
+
+
+def generic_query_cursor(cursor, table, qcol_list, col, optype, colval):
+    conv = op_conv(optype, colval)
+    qcols = ", ".join(qcol_list)
+    query = "SELECT " + qcols + " FROM " + table + " WHERE " + col + conv['op']
+    if isinstance(colval, (str, bytes)):
+        query += "\"" + conv['val'] + "\""
+    else:
+        query += str(conv['val'])
+    query += ";"
+    cursor.execute(query)
+    return cursor
+
+
+def generic_query_fetchone(cursor, table, qcol_list, col, optype, colval):
+    return generic_query_cursor(
+        cursor, table, qcol_list, col, optype, colval
+    ).fetchone()
+
+
+def generic_query_sets(cursor, table, qcol_list, col, optype, colval, num):
+    cursor = generic_query_cursor(cursor, table, qcol_list, col, optype, colval)
+    cursor.arraysize = num
+    return cursor
+
+
+def generic_query_fetchall(cursor, table, qcol_list, col, optype, colval):
+    return generic_query_cursor(
+        cursor, table, qcol_list, col, optype, colval
+    ).fetchall()
+
+
 def from_typeid(cursor, typeID, colNameList):
-    colNames = ', '.join(colNameList)
-    (val,) = generic_query_fetchone(cursor, "invTypes", colNames, "typeID",
+    (val,) = generic_query_fetchone(cursor, "invTypes", colNameList, "typeID",
                                     typeID)
     return val
 
@@ -11,14 +50,3 @@ def typename_from_typeid(cursor, typeID):
 
 def volume_from_typeid(cursor, typeID):
     return from_typeid(cursor, typeID, ['volume'])
-
-
-def generic_query_fetchone(cursor, table, qcol, col, colval):
-    query = "SELECT " + qcol + " FROM " + table + " WHERE " + col + "="
-    if isinstance(colval, (str, bytes)):
-        query += "\"" + colval + "\""
-    else:
-        query += str(colval)
-    query += ";"
-    cursor.execute(query)
-    return cursor.fetchone()
